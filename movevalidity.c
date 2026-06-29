a6636cf945711e7f267a415592f982e1cd9d344a
Ali <ali.a.abdullah.010@gmail.com>
2026-06-29 19:16:03 -0400

    Fixing array indexing issues in the bishop function

--- movevalidity.c
+++ movevalidity.c
@@ -211,7 +211,7 @@ int bishop_check(int x1, int y1, int x2, int y2) {
     int dist; //same structure as the rook_check
     for (dist = 2*abs(x2-x1); dist>0;dist-=2) {
         //the for loop could be reduced, but symbolically represents both the Manhattan (|x|+|y|) distance
-        if (board[x1+sign(x2-x1)*dist][y1+sign(y2-y1)*dist]!='x') {
+        if (board[x1+sign(x2-x1)*(dist/2)][y1+sign(y2-y1)*(dist/2)]!='x') {
             printf("%d\n",x1+sign(x2-x1)*dist);
             return 0;
         }
